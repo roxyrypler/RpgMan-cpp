42e630ceef20afd09e6a9761e89322d072c1cbb5
Patrick <30288569+roxyrypler@users.noreply.github.com>
2021-07-09 11:51:43 +0200

    Linking and setting up sfml to vs solution and testing includes

--- RpgMan/RpgMan/RpgMan.cpp
+++ RpgMan/RpgMan/RpgMan.cpp
@@ -1,6 +1,13 @@
 
 #include <iostream>
 
+#include<SFML/Graphics.hpp>
+#include<SFML/System.hpp>
+#include<SFML/Window.hpp>
+#include<SFML/Audio.hpp>
+#include<SFML/Network.hpp>
+
+
 int main()
 {
     std::cout << "Hello World!\n";
